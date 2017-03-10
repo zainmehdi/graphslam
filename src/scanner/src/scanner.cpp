924f930cf910c25dabf831d5ddfe1346815170eb
Joan Sola <jsola@iri.upc.edu>
2017-03-10 10:47:40 +0100

    Organizing functions and configuring GICP

--- src/scanner/src/scanner.cpp
+++ src/scanner/src/scanner.cpp
@@ -7,67 +7,49 @@ ros::ServiceClient keyframe_last_client;
 ros::ServiceClient keyframe_closest_client;
 
 // Tuning constants:
-const double converged_fitness_threshold = 0.1; // TODO migrate to rosparams
+const double converged_fitness_threshold = 0.3; // TODO migrate to rosparams
 double k_disp_disp = 0.1, k_rot_disp = 0.1, k_rot_rot = 0.1; // TODO migrate to rosparams
 
-sensor_msgs::PointCloud2 scan_to_pointcloud(sensor_msgs::LaserScan input) {
-//  ROS_INFO("SCAN TO POINTCLOUD STARTED.");
-  laser_geometry::LaserProjection projector;
-  sensor_msgs::PointCloud2 output;
-  projector.projectLaser(input, output);
+    // init GICP algorithm
+    pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> gicp;
 
-//  ROS_INFO("SCAN TO POINTCLOUD FINISHED.");
-  return output;
-}
-
-pcl::PointCloud<pcl::PointXYZ>::Ptr format_pointcloud(sensor_msgs::PointCloud2 input) {
-//  ROS_INFO("FORMAT POINTCLOUD STARTED.");
-  pcl::PCLPointCloud2 pcl2_pointcloud;
-  pcl_conversions::toPCL(input, pcl2_pointcloud);
-
-  pcl::PointCloud<pcl::PointXYZ>::Ptr output(new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::fromPCLPointCloud2(pcl2_pointcloud, *output);
-  
-//  ROS_INFO("FORMAT POINTCLOUD FINISHED.");
-  return output;
-}
-
-common::Registration gicp(sensor_msgs::PointCloud2 input_1, sensor_msgs::PointCloud2 input_2) {
+common::Registration gicp_register(sensor_msgs::PointCloud2 input_1, sensor_msgs::PointCloud2 input_2) {
 //  ROS_INFO("GICP STARTED.");
-  pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud_1 = format_pointcloud(input_1);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud_2 = format_pointcloud(input_2);
-  
-  pcl::GeneralizedIterativeClosestPoint<pcl::PointXYZ, pcl::PointXYZ> gicp;
 
+
+
+  // assign inputs
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud_1 = format_pointcloud(input_1);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud_2 = format_pointcloud(input_2);
   gicp.setInputSource(pointcloud_1);
   gicp.setInputTarget(pointcloud_2);
 
+  // align
   pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud_transform(new pcl::PointCloud<pcl::PointXYZ>);
   gicp.align(*pointcloud_transform);
 
-  bool converged = gicp.hasConverged();
-  double converged_fitness = gicp.getFitnessScore();
-  Eigen::Matrix4f transform = gicp.getFinalTransformation();
-  geometry_msgs::Pose2D transform_Delta = make_Delta(transform);
-
   common::Registration output;
-  output.keyframe_flag = false;
 
-  if (converged)
+  output.keyframe_flag = false;
+  if (gicp.hasConverged())
   {
+      // Get transformation Delta and compute its covariance
+      Eigen::Matrix4f transform = gicp.getFinalTransformation();
+      geometry_msgs::Pose2D transform_Delta = make_Delta(transform);
+
       Eigen::MatrixXd covariance_Delta = compute_covariance(k_disp_disp, k_rot_disp, k_rot_rot, transform_Delta);
       common::Pose2DWithCovariance Delta = create_Pose2DWithCovariance_msg(transform_Delta, covariance_Delta);
 
-      output.factor_new.delta = Delta;
+      // Assign to outputs
+      output.factor_new.delta  = Delta;
       output.factor_loop.delta = Delta;
 
-      if (converged_fitness > converged_fitness_threshold)
+      if (gicp.getFitnessScore() > converged_fitness_threshold)
       {
           output.keyframe_flag = true;
       }
   }
 
-  //  ROS_INFO("GICP FINISHED.");
   return output;
 }
 
@@ -100,15 +82,11 @@ void scanner_callback(const sensor_msgs::LaserScan& input) {
   if(keyframe_last_request_returned) {
     sensor_msgs::PointCloud2 input_pointcloud = scan_to_pointcloud(input);
     sensor_msgs::PointCloud2 keyframe_last_pointcloud = keyframe_last_request.response.keyframe_last.pointcloud;
-    //    keyframe_IDs = keyframe_last_request.response.keyframe_last.id + 1;
     
-    //    ROS_INFO("GICP registration_last STARTED");
-    common::Registration registration_last = gicp(input_pointcloud, keyframe_last_pointcloud);
-    //    ROS_INFO("GICP registration_last FINISHED");
+    common::Registration registration_last = gicp_register(input_pointcloud, keyframe_last_pointcloud);
     
     output.keyframe_flag = registration_last.keyframe_flag;
     output.loop_closure_flag = false;
-    // output.keyframe_new.id = keyframe_IDs; // we do not set new IDs here
     output.keyframe_new.ts = input.header.stamp;
     output.keyframe_new.pointcloud = input_pointcloud;
     output.keyframe_new.scan = input;
@@ -129,7 +107,7 @@ void scanner_callback(const sensor_msgs::LaserScan& input) {
 	sensor_msgs::PointCloud2 keyframe_closest_pointcloud =
 	  keyframe_closest_request.response.keyframe_closest.pointcloud;
 
-	common::Registration registration_closest = gicp(keyframe_closest_pointcloud, keyframe_last_pointcloud);
+	common::Registration registration_closest = gicp_register(keyframe_closest_pointcloud, keyframe_last_pointcloud);
 
 	// compute factor things
 	output.loop_closure_flag = true;
@@ -160,6 +138,11 @@ int main(int argc, char** argv) {
 
   keyframe_last_client = n.serviceClient<common::LastKeyframe>("/graph/last_keyframe");
   keyframe_closest_client = n.serviceClient<common::ClosestKeyframe>("/graph/closest_keyframe");
+
+  // Setup GICP algorithm
+  ROS_INFO("gicp.getUseReciprocalCorrespondences() = %d", gicp.getUseReciprocalCorrespondences());
+  gicp.setUseReciprocalCorrespondences(true);
+  ROS_INFO("gicp.getUseReciprocalCorrespondences() = %d", gicp.getUseReciprocalCorrespondences());
 
   ros::spin();
   return 0;
