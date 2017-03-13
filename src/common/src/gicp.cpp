a4895eca473989020c43fb72bbe32724cfcfc85b
Joan Sola <jsola@iri.upc.edu>
2017-03-13 17:19:16 +0100

    Add robot_0 ground truth

--- src/common/src/gicp.cpp
+++ src/common/src/gicp.cpp
@@ -12,6 +12,7 @@ Eigen::Matrix4f carry_transform;
 sensor_msgs::LaserScan robot_0_laserscan;
 sensor_msgs::LaserScan robot_1_laserscan;
 nav_msgs::Odometry robot_1_gt;
+nav_msgs::Odometry robot_0_gt;
 
 void gicp_register(sensor_msgs::PointCloud2 input_1,
 				   sensor_msgs::PointCloud2 input_2,
@@ -31,38 +32,26 @@ void gicp_register(sensor_msgs::PointCloud2 input_1,
     std::cout << "An exception occurred. Exception Nr. " << e << '\n';
   }
 
-  common::Registration output;
-
   ROS_INFO("#############################");
-  ROS_INFO("Converged?: %d, Fitness Score: %f", gicp.hasConverged(), gicp.getFitnessScore());
 
   transform = gicp.getFinalTransformation();
   geometry_msgs::Pose2D transform_Delta = make_Delta(transform);
 
   Eigen::MatrixXd covariance_Delta = compute_covariance(k_disp_disp, k_rot_disp, k_rot_rot, transform_Delta);
   common::Pose2DWithCovariance Delta = create_Pose2DWithCovariance_msg(transform_Delta, covariance_Delta);
-  ROS_INFO("dx = %f; dy = %f; dth = %f", Delta.pose.x, Delta.pose.y, Delta.pose.theta);
-  ROS_INFO("[%f %f %f %f %f %f %f %f %f]", Delta.covariance[0],
-	   Delta.covariance[1],
-	   Delta.covariance[2],
-	   Delta.covariance[3],
-	   Delta.covariance[4],
-	   Delta.covariance[5],
-	   Delta.covariance[6],
-	   Delta.covariance[7],
-	   Delta.covariance[8]);
-//  ROS_INFO("####GROUNG TRUTH#####");
-  ROS_INFO("x1 = %f; y1 = %f; th1 = %f", robot_1_gt.pose.pose.position.x, robot_1_gt.pose.pose.position.y, tf::getYaw(robot_1_gt.pose.pose.orientation));
-//  ROS_INFO("[%f %f %f %f %f %f %f %f %f]", robot_1_gt.pose.covariance[0],
-//	   robot_1_gt.pose.covariance[1],
-//	   robot_1_gt.pose.covariance[2],
-//	   robot_1_gt.pose.covariance[3],
-//	   robot_1_gt.pose.covariance[4],
-//	   robot_1_gt.pose.covariance[5],
-//	   robot_1_gt.pose.covariance[6],
-//	   robot_1_gt.pose.covariance[7],
-//	   robot_1_gt.pose.covariance[8]);
-//  ROS_INFO("####GROUNG TRUTH#####");
+  ROS_INFO("Converged?: %d, Fitness Score: %f", gicp.hasConverged(), gicp.getFitnessScore());
+  ROS_INFO("GT: x0 = %f; y1 = %f; th1 = %f", robot_0_gt.pose.pose.position.x, robot_0_gt.pose.pose.position.y, tf::getYaw(robot_0_gt.pose.pose.orientation));
+  ROS_INFO("GT: x1 = %f; y1 = %f; th1 = %f", robot_1_gt.pose.pose.position.x, robot_1_gt.pose.pose.position.y, tf::getYaw(robot_1_gt.pose.pose.orientation));
+  ROS_INFO("RG: dx = %f; dy = %f; dth = %f", Delta.pose.x, Delta.pose.y, Delta.pose.theta);
+//  ROS_INFO("[%f %f %f %f %f %f %f %f %f]", Delta.covariance[0],
+//	   Delta.covariance[1],
+//	   Delta.covariance[2],
+//	   Delta.covariance[3],
+//	   Delta.covariance[4],
+//	   Delta.covariance[5],
+//	   Delta.covariance[6],
+//	   Delta.covariance[7],
+//	   Delta.covariance[8]);
   double end = ros::Time::now().toSec();
   ROS_INFO("Time to Completion: %f seconds", end - start);
   ROS_INFO("#############################");
@@ -84,6 +73,11 @@ void robot_1_scanner_callback(const sensor_msgs::LaserScan& input) {
   ROS_INFO("robot_1_laserscan received.");
 }
 
+void robot_0_gt_callback(const nav_msgs::Odometry& input) {
+  robot_0_gt = input;
+  ROS_INFO("robot_0_gt received.");
+}
+
 void robot_1_gt_callback(const nav_msgs::Odometry& input) {
   robot_1_gt = input;
   ROS_INFO("robot_1_gt received.");
@@ -96,7 +90,8 @@ int main(int argc, char** argv) {
 
   ros::Subscriber robot_0_scan_sub = n.subscribe("/robot_0/base_scan", 1, robot_0_scanner_callback);
   ros::Subscriber robot_1_scan_sub = n.subscribe("/robot_1/base_scan", 1, robot_1_scanner_callback);
-  ros::Subscriber ground_truth_sub = n.subscribe("/robot_1/base_pose_ground_truth", 1, robot_1_gt_callback);
+  ros::Subscriber ground_truth_0_sub = n.subscribe("/robot_0/base_pose_ground_truth", 1, robot_0_gt_callback);
+  ros::Subscriber ground_truth_1_sub = n.subscribe("/robot_1/base_pose_ground_truth", 1, robot_1_gt_callback);
   delta_pub = n.advertise<common::Pose2DWithCovariance>("/scanner/delta", 1);
 
   // Setup GICP algorithm
